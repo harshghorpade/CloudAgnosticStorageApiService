4711f5d3fd7a79f39e66bf9f6dd16c4926047d9a
Harshavardhan Ghorpade <simplyharsh7@gmail.com>
2020-06-14 12:55:34 +0530

    Update ApiRequestModel.cs

--- src/Models/ApiRequestModel.cs
+++ src/Models/ApiRequestModel.cs
@@ -1,13 +1,18 @@
 ﻿// ==================================================
-// Service Interface
+// API Request Object Model
 // ==================================================
 
 namespace CloudAgnosticStorageAPI.Models
 {
     public class uploadDownloadInput
     {
+        // file name input for upload or download file
         public string fileName { get; set; }
+        
+        // storage service type option
         public string storageServiceType { get; set; }
+        
+        // GET or PUT Pre-signed URL
         public string signedUrl { get; set; }
     }
 }
